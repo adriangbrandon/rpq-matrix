a6f6a0bcfbf7ae626d213cc9eb17b08d96962d3c
Adrián Gómez Brandón <adriangbrandon@gmail.com>
2022-06-22 18:08:45 +0200

    New heuristics

--- src/selectivity.hpp
+++ src/selectivity.hpp
@@ -53,7 +53,7 @@ namespace selectivity {
     }
 
     struct info {
-        uint64_t weight;
+        double weight;
         split_type  split;
     };
 
@@ -189,7 +189,7 @@ namespace selectivity {
         info intersection(const uint64_t ith) {
             info res;
             res.split = intersect;
-            uint64_t base, right, left;
+            double base, right, left;
             if(m_s[ith+1] < m_t[ith]){
                 base = m_s[ith+1];
             }else{
@@ -200,46 +200,97 @@ namespace selectivity {
             res.weight = left * base + base * right;
             return res;
         }
-        /*info simple(const uint64_t id,
-                    const bwt_nose &L_S, const bwt_type &wt_pred_s,
-                    uint64_t maxP, uint64_t sigma){
-            auto e_d = L_S.get_C(id + 1)-1;
-            auto b_d = L_S.get_C(id);
-            auto rev_id = reverse(id, maxP);
-            auto e_r = L_S.get_C(rev_id + 1)-1;
-            auto b_r = L_S.get_C(rev_id);
+
+    };
+
+    class h_distsigma_path {
+
+
+    private:
+        std::vector<double> m_s;
+        std::vector<double> m_t;
+        std::vector<double> m_r;
+        std::vector<double> m_l;
+        uint64_t m_max_p;
+        uint64_t m_sigma;
+
+
+    public:
+        h_distsigma_path(const std::vector<PairPredPos> &preds,
+                        const bwt_nose &L_S, const bwt_type &wt_pred_s,
+                        uint64_t maxP, uint64_t sigma){
+
+            m_max_p = maxP;
+            m_sigma = sigma;
+            //m_t.push_back(-1ULL);
+            for(const auto &pair : preds){
+
+                auto e_d = L_S.get_C(pair.id_pred + 1)-1;
+                auto b_d = L_S.get_C(pair.id_pred);
+                auto v_target = distinct_values(b_d, e_d, wt_pred_s);
+                m_t.push_back(v_target / (double) m_sigma);
+
+                auto rev_id = reverse(pair.id_pred, m_max_p);
+                auto e_r = L_S.get_C(rev_id + 1)-1;
+                auto b_r = L_S.get_C(rev_id);
+                auto v_source = distinct_values(b_r, e_r, wt_pred_s);
+                m_s.push_back(v_source / (double) m_sigma);
+            }
+            // m_s.push_back(-1ULL);
+            auto s = m_s.size()+1;
+            m_r.resize(s);
+            m_r[s-1]=1;
+            m_l.resize(s);
+            m_l[0]=1;
+            for(uint64_t i = 1; i < s; ++i){
+                m_r[s-i-1] = m_r[s-i] * m_t[m_t.size()-i];
+                m_l[i] = m_l[i-1] * m_s[i-1];
+            }
+            std::cout << "-----T-----" << std::endl;
+            printVector(m_t);
+            std::cout << "-----S-----" << std::endl;
+            printVector(m_s);
+            std::cout << "-----L-----" << std::endl;
+            printVector(m_l);
+            std::cout << "-----R-----" << std::endl;
+            printVector(m_r);
+
+
+        }
+
+        info simple(const uint64_t ith){
             info res;
-            auto v_target = distinct_values(b_d, e_d, wt_pred_s);
-            auto v_source = distinct_values(b_r, e_r, wt_pred_s);
-            std::cout << "Dist-source: " << v_source << " Length-source: " << (e_d-b_d)+1 << std::endl;
-            std::cout << "Dist-target: " << v_target << " Length-source: " << (e_r-b_r)+1 << std::endl;
-            if(v_source > v_target){
-                res.split = target;
-                res.weight = v_target / (double) sigma;
-            }else{
+            //double a;
+            if(m_s[ith] < m_t[ith]){
                 res.split = source;
-                res.weight = v_source / (double) sigma;
+                if(ith == 0){
+                    res.weight = m_s[ith] * m_r[ith+1];
+                }else{
+                    res.weight = m_s[ith] * m_r[ith+1] + m_s[ith] * m_l[ith-1];
+                }
+            }else{
+                res.split = target;
+                if(ith == m_t.size()-1){
+                    res.weight = m_t[ith] * m_l[ith-1];
+                }else{
+                    res.weight = m_t[ith] * m_l[ith-1] + m_t[ith] * m_r[ith+1];
+                }
+
             }
             return res;
         }
 
-        info intersection(const uint64_t id1, const uint64_t id2,
-                          const bwt_nose &L_S, const bwt_type &wt_pred_s,
-                          uint64_t maxP, uint64_t sigma){
-            auto e_l = L_S.get_C(id1 + 1)-1;
-            auto b_l = L_S.get_C(id1);
-            auto rev_id2 = reverse(id2, maxP);
-            auto e_r = L_S.get_C(rev_id2 + 1)-1;
-            auto b_r = L_S.get_C(rev_id2);
-            double v_l = distinct_values(b_l, e_l, wt_pred_s);
-            double v_r = distinct_values(b_r, e_r, wt_pred_s);
-            std::cout << "Dist-left: " << v_l << " Length-left: " << (e_l-b_l)+1 << std::endl;
-            std::cout << "Dist-right: " << v_r << " Length-right: " << (e_r-b_r)+1 << std::endl;
+        info intersection(const uint64_t ith) {
             info res;
-            res.weight = (v_l * v_r) / (double) (sigma * sigma);
             res.split = intersect;
+            double base, right, left;
+            base = m_s[ith+1] * m_t[ith];
+            right = m_r[ith+2];
+            left = m_l[ith];
+            res.weight = left * base + base * right;
             return res;
-        }*/
+        }
+
     };
 
     struct h_ratio {
