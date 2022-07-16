e90fcf4fcccc31ef28beca8155ad14df5a8324cc
Adrián Gómez Brandón <adriangbrandon@gmail.com>
2022-07-16 21:07:12 +0200

    V3 using two B_arrays

--- src/query-index-split-v3-all.cpp
+++ src/query-index-split-v3-all.cpp
@@ -60,6 +60,7 @@ int main(int argc, char **argv) {
     bool flag_s, flag_o, skip_flag;
     std::vector<std::pair<uint64_t, uint64_t>> query_output;
     std::vector<word_t> B_array(4 * graph.n_labels(), 0);
+    std::vector<word_t> B_array2(4 * graph.n_labels(), 0);
 
     high_resolution_clock::time_point start, stop;
     double total_time = 0.0;
@@ -225,7 +226,7 @@ int main(int argc, char **argv) {
                     else if (is_or and n_predicates > 3)
                         graph.or_query_var_to_var(query, n_predicates, bound, pred_map, query_output);
                     else
-                        graph.rpq_var_to_var_split(query, pred_map, B_array, query_output, n_predicates,
+                        graph.rpq_var_to_var_split(query, pred_map, B_array, B_array2, query_output, n_predicates,
                                                is_negated_pred, n_operators, is_a_path);
                 } else {
                     if (flag_s) {
