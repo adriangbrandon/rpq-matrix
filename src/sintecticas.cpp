9c436c820afc0b237b52f04db31d826d32f8dd8f
adrian <adriangbrandon@gmail.com>
2024-04-01 15:29:31 +0200

    Skipping timeout

--- src/sintecticas.cpp
+++ src/sintecticas.cpp
@@ -33,69 +33,63 @@ int main(int argc, char **argv) {
     uint64_t sum = 0;
 
     std::cout << "Sum..." << std::flush;
-    auto t1 = std::chrono::high_resolution_clock::now();
     for(uint i = 1; i < 20; ++i){
-        time_beg();
+        auto t1 = std::chrono::high_resolution_clock::now();
         M = matSum(m_matrices[i-1], m_matrices[i]);
         sum += M->elems;
+        auto t2 =  std::chrono::high_resolution_clock::now();
+        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();
+        std::cerr << ns << std::endl;
     }
-    auto t2 =  std::chrono::high_resolution_clock::now();
-    auto sum_ns = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
-    double avg_sum = sum_ns / 19.0;
     std::cout << " done. [" << sum << "]" << std::endl;
 
     std::cout << "Sum RC..." << std::flush;
     sum = 0;
-    t1 = std::chrono::high_resolution_clock::now();
     for(uint i = 1; i < 20; ++i){
-        time_beg();
+        auto t1 = std::chrono::high_resolution_clock::now();
         M = matSum1(fullSide, m_matrices[i-1], m_matrices[i], fullSide);
         sum += M->elems;
+        auto t2 =  std::chrono::high_resolution_clock::now();
+        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();
+        std::cerr << ns << std::endl;
     }
-    t2 =  std::chrono::high_resolution_clock::now();
-    auto sum_rec_ns = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
-    double avg_sum_rec = sum_rec_ns / 19.0;
     std::cout << " done. [" << sum << "]" << std::endl;
 
     std::cout << "And..." << std::flush;
     sum = 0;
-    t1 = std::chrono::high_resolution_clock::now();
     for(uint i = 1; i < 20; ++i){
-        time_beg();
+        auto t1 = std::chrono::high_resolution_clock::now();
         M = matAnd(m_matrices[i-1], m_matrices[i]);
         sum += M->elems;
+        auto t2 =  std::chrono::high_resolution_clock::now();
+        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();
+        std::cerr << ns << std::endl;
     }
-    t2 =  std::chrono::high_resolution_clock::now();
-    auto intersect_ns = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
-    double avg_intersect = intersect_ns/19.0;
     std::cout << " done. [" << sum << "]" << std::endl;
 
     std::cout << "Mult..." << std::flush;
     sum = 0;
-    t1 = std::chrono::high_resolution_clock::now();
     for(uint i = 1; i < 20; ++i){
-        time_beg();
+        auto t1 = std::chrono::high_resolution_clock::now();
         M = matMult(m_matrices[i-1], m_matrices[i]);
         sum += M->elems;
+        auto t2 =  std::chrono::high_resolution_clock::now();
+        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();
+        std::cerr << ns << std::endl;
     }
-    t2 =  std::chrono::high_resolution_clock::now();
-    auto product_ns = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
-    double avg_product = product_ns/19.0;
     std::cout << " done. [" << sum << "]" << std::endl;
 
     std::cout << "Closure..." << std::flush;
     sum = 0;
-    t1 = std::chrono::high_resolution_clock::now();
     for(uint i = 0; i < 20; ++i){
-        time_beg();
+        auto t1 = std::chrono::high_resolution_clock::now();
         M = matClos(m_matrices[i], 1);
         sum += M->elems;
+        auto t2 =  std::chrono::high_resolution_clock::now();
+        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();
+        std::cerr << ns << std::endl;
     }
-    t2 =  std::chrono::high_resolution_clock::now();
-    auto closure_ns = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
-    double avg_closure = closure_ns / 20.0;
     std::cout << " done. [" << sum << "]" << std::endl;
 
-    std::cerr << avg_sum << ";" << avg_sum_rec << ";" << avg_intersect << ";"<< avg_product << ";" << avg_closure << std::endl;
 
 }
